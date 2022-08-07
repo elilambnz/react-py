72ea224d5aa9b2924976b83d4b587f4dc0992676
Eli Lamb <eli@candelic.com>
2022-08-07 19:01:34 +1200

    Updated example styling

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,10 +9,9 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
+import clsx from "clsx";
 
-const placeholder = `
-print("Hello World")
-`;
+const placeholder = `print("Hello World")`;
 
 function App() {
   const [input, setInput] = useState(placeholder);
@@ -88,13 +87,13 @@ function App() {
             </Popover.Panel>
           </Transition>
         </Popover>
-        <main className="pb-16">
+        <main>
           <div className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
             <div className="mx-auto max-w-7xl lg:px-8">
               <div className="lg:grid lg:grid-cols-2 lg:gap-8">
                 <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
-                  <div className="lg:py-24">
-                    <h1 className="mt-4 text-4xl font-extrabold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                  <div>
+                    <h1 className="text-4xl font-extrabold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
                       <span className="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-transparent sm:pb-5">
                         react-py
                       </span>
@@ -108,11 +107,11 @@ function App() {
             </div>
           </div>
 
-          <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
+          <div className="relative bg-gray-50 pt-16 pb-16 sm:pt-24 lg:pt-32">
             <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
               <div>
                 <h2 className="text-base font-semibold uppercase tracking-wider text-cyan-600">
-                  Example
+                  Basic example
                 </h2>
               </div>
 
@@ -129,27 +128,90 @@ function App() {
                 </div>
 
                 <div className="ml-4 w-1/2 text-left">
-                  {isLoading ? <p>Loading...</p> : <p>Ready!</p>}
+                  <h2 className="text-base font-semibold uppercase tracking-wider text-cyan-600">
+                    {isLoading ? "Loading..." : "Ready!"}
+                  </h2>
 
                   <button
                     type="button"
-                    className="mt-4 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:cursor-pointer hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className={clsx(
+                      "mt-4 inline-flex items-center rounded-md border border-transparent bg-cyan-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:cursor-pointer hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2",
+                      (isLoading || isRunning) &&
+                        "cursor-not-allowed opacity-50"
+                    )}
                     disabled={isLoading || isRunning}
                     onClick={() => runPython(input)}
                   >
                     {!isRunning ? "Run" : "Running..."}
                   </button>
                   <div className="mt-4">
-                    <p>Output</p>
-                    <pre>{stdout}</pre>
-                    <p>Error</p>
-                    <pre>{stderr}</pre>
+                    <p>
+                      <b>Output</b>
+                    </p>
+                    <pre className="mt-2 min-h-[2.5rem] rounded-md bg-slate-200 p-2">
+                      <code>{stdout}</code>
+                    </pre>
+                    <br />
+                    <p>
+                      <b>Error</b>
+                    </p>
+                    <pre className="mt-2 min-h-[2.5rem] rounded-md bg-slate-200 p-2 text-red-500">
+                      <code>{stderr}</code>
+                    </pre>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </main>
+
+        <footer className="bg-white">
+          <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+            <div className="flex justify-center space-x-6 md:order-2">
+              <a
+                href="https://github.com/elilambnz/react-py"
+                className="text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">GitHub</span>
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  className="h-6 w-6"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="mt-8 md:order-1 md:mt-0">
+              <p className="text-center text-base text-gray-400">
+                Made with ❤️ + ☕️ by{" "}
+                <a
+                  href="https://github.com/elilambnz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-400 hover:text-gray-500"
+                >
+                  Eli Lamb
+                </a>
+                . Licensed under the{" "}
+                <a
+                  href="https://github.com/elilambnz/react-py/blob/main/LICENSE.md"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-400 hover:text-gray-500"
+                >
+                  MIT License
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   );
