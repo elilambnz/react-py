e144415cbbd98d5c4bf52fc94241184d7bb686b0
Eli Lamb <eli@candelic.com>
2022-08-07 17:53:08 +1200

    Example website for gh-pages

---
+++ example/src/App.tsx
@@ -0,0 +1,160 @@
+import { useState } from "react";
+import { usePython } from "react-py";
+
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+const placeholder = `
+print("Hello World")
+`;
+
+function App() {
+  const [input, setInput] = useState(placeholder);
+  const { runPython, stdout, stderr, isLoading } = usePython();
+
+  return (
+    <div className="min-w-full bg-white">
+      <div className="relative overflow-hidden">
+        <Popover as="header" className="relative">
+          <div className="bg-gray-900 pt-6">
+            <nav
+              className="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
+              aria-label="Global"
+            >
+              <div className="flex flex-1 items-center">
+                <div className="flex w-full items-center justify-between md:w-auto">
+                  <div className="-mr-2 flex items-center md:hidden">
+                    <Popover.Button className="focus-ring-inset inline-flex items-center justify-center rounded-md bg-gray-900 p-2 text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:flex md:items-center md:space-x-6">
+                <a
+                  href="https://github.com/elilambnz/react-py"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
+                >
+                  View on GitHub
+                </a>
+              </div>
+            </nav>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute inset-x-0 top-0 origin-top transform p-2 transition md:hidden"
+            >
+              <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+                <div className="flex items-center justify-between px-5 pt-4">
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="pt-5 pb-6">
+                  <div className="mt-6 px-5">
+                    <a
+                      href="https://github.com/elilambnz/react-py"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700"
+                    >
+                      View on GitHub
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+        <main className="pb-16">
+          <div className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
+            <div className="mx-auto max-w-7xl lg:px-8">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
+                  <div className="lg:py-24">
+                    <h1 className="mt-4 text-4xl font-extrabold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                      <span className="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-transparent sm:pb-5">
+                        react-py
+                      </span>
+                    </h1>
+                    <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+                      Run Python code directly in the browser.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
+            <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+              <div>
+                <h2 className="text-base font-semibold uppercase tracking-wider text-cyan-600">
+                  Example
+                </h2>
+              </div>
+
+              <div className="mt-8 flex rounded-md border border-black p-4">
+                <div className="w-1/2">
+                  <AceEditor
+                    value={placeholder}
+                    mode="python"
+                    theme="github"
+                    onChange={(newValue) => setInput(newValue)}
+                    name="codeblock"
+                    editorProps={{ $blockScrolling: true }}
+                  />
+                </div>
+
+                <div className="ml-4 w-1/2 text-left">
+                  {isLoading ? <p>Loading...</p> : <p>Ready!</p>}
+
+                  <button
+                    type="button"
+                    className="mt-4 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:cursor-pointer hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    // disabled={isLoading || isRunning}
+                    disabled={isLoading}
+                    onClick={() => runPython(input)}
+                  >
+                    {/* {!isRunning ? "Run" : "Running..."} */}
+                    Run
+                  </button>
+                  <div className="mt-4">
+                    <p>Output</p>
+                    <pre>{stdout}</pre>
+                    <p>Error</p>
+                    <pre>{stderr}</pre>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
