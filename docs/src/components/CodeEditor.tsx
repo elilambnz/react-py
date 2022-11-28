431750c6f019bc22b1b112dd8dd7f1d2696dcc56
Eli Lamb <eli@pixellab.nz>
2022-11-29 11:20:28 +1300

    Docs update

--- docs/src/components/CodeEditor.tsx
+++ docs/src/components/CodeEditor.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import { useColorMode } from "@docusaurus/theme-common";
-import { usePython } from "react-py";
 import clsx from "clsx";
 
 const editorOptions = {
@@ -26,19 +25,7 @@ export default function CodeEditor(props) {
     setShowOutput(false);
   }, [code]);
 
-  const { runPython, stdout, stderr, isLoading, isRunning } = usePython();
-
   const { colorMode } = useColorMode();
-
-  function run() {
-    setShowOutput(true);
-    runPython(input);
-  }
-
-  function reset() {
-    setShowOutput(false);
-    setInput(code.trimEnd());
-  }
 
   return (
     <div className="relative mb-10">
@@ -68,40 +55,62 @@ export default function CodeEditor(props) {
         }}
       </BrowserOnly>
 
-      <span className="absolute top-2 right-2 z-10 inline-flex rounded-md shadow-sm">
-        <button
-          onClick={run}
-          type="button"
-          disabled={isLoading || isRunning}
-          className={clsx(
-            "relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
-            !(isLoading || isRunning)
-              ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
-              : "opacity-50"
-          )}
-        >
-          Run
-        </button>
-        <button
-          onClick={reset}
-          type="button"
-          disabled={isRunning}
-          className={clsx(
-            "relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
-            !isRunning
-              ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
-              : "opacity-50"
-          )}
-        >
-          Reset
-        </button>
-      </span>
-      {showOutput && (
-        <pre className="mt-4 text-left">
-          <code>{stdout}</code>
-          <code className="text-red-500">{stderr}</code>
-        </pre>
-      )}
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => {
+          const { usePython } = require("react-py");
+
+          const { runPython, stdout, stderr, isLoading, isRunning } =
+            usePython();
+
+          function run() {
+            setShowOutput(true);
+            runPython(input);
+          }
+
+          function reset() {
+            setShowOutput(false);
+            setInput(code.trimEnd());
+          }
+          return (
+            <>
+              <span className="absolute top-2 right-2 z-10 inline-flex rounded-md shadow-sm">
+                <button
+                  onClick={run}
+                  type="button"
+                  disabled={isLoading || isRunning}
+                  className={clsx(
+                    "relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
+                    !(isLoading || isRunning)
+                      ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
+                      : "opacity-50"
+                  )}
+                >
+                  Run
+                </button>
+                <button
+                  onClick={reset}
+                  type="button"
+                  disabled={isRunning}
+                  className={clsx(
+                    "relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
+                    !isRunning
+                      ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
+                      : "opacity-50"
+                  )}
+                >
+                  Reset
+                </button>
+              </span>
+              {showOutput && (
+                <pre className="mt-4 text-left">
+                  <code>{stdout}</code>
+                  <code className="text-red-500">{stderr}</code>
+                </pre>
+              )}
+            </>
+          );
+        }}
+      </BrowserOnly>
     </div>
   );
 }
