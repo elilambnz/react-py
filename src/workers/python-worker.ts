6762eba0b1a02f8cd86f508093ef897241cb9ec4
Eli Lamb <eli@pixellab.nz>
2022-12-08 11:11:08 +1300

    Convert worker from js to ts

---
+++ src/workers/python-worker.ts
@@ -0,0 +1,29 @@
+// @ts-expect-error
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
+
+declare global {
+  interface Window {
+    loadPyodide: any;
+    pyodide: any;
+  }
+}
+
+import { expose } from "comlink";
+
+const python = {
+  async init(
+    id: string,
+    stdout: (id: string, msg: string) => void,
+    onLoad: (version: string) => void
+  ) {
+    self.pyodide = await self.loadPyodide({
+      stdout: (msg: string) => stdout(id, msg),
+    });
+    onLoad(self.pyodide.version);
+  },
+  async run(code: string) {
+    await self.pyodide.runPythonAsync(code);
+  },
+};
+
+expose(python);
