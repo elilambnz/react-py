572507fe4e35e80acc656cca8ec86e4d7c3cf906
Eli Lamb <eli@pixellab.nz>
2022-12-08 15:13:16 +1300

    Added interruptExecution control to CodeEditor

--- website/src/components/CodeEditor.tsx
+++ website/src/components/CodeEditor.tsx
@@ -15,7 +15,11 @@ const editorOnLoad = (editor) => {
   editor.moveCursorTo(0, 0);
 };
 
-export default function CodeEditor(props) {
+interface CodeEditorProps {
+  code: string;
+}
+
+export default function CodeEditor(props: CodeEditorProps) {
   const { code } = props;
   const [input, setInput] = useState(code.trimEnd());
   const [showOutput, setShowOutput] = useState(false);
@@ -26,6 +30,19 @@ export default function CodeEditor(props) {
   }, [code]);
 
   const { colorMode } = useColorMode();
+
+  function onRun() {
+    setShowOutput(true);
+  }
+
+  function onStop() {
+    setShowOutput(false);
+  }
+
+  function onReset() {
+    setShowOutput(false);
+    setInput(code.trimEnd());
+  }
 
   return (
     <div className="relative mb-10">
@@ -56,61 +73,103 @@ export default function CodeEditor(props) {
       </BrowserOnly>
 
       <BrowserOnly fallback={<div>Loading...</div>}>
-        {() => {
-          const { usePython } = require("react-py");
-
-          const { runPython, stdout, stderr, isLoading, isRunning } =
-            usePython();
-
-          function run() {
-            setShowOutput(true);
-            runPython(input);
-          }
-
-          function reset() {
-            setShowOutput(false);
-            setInput(code.trimEnd());
-          }
-          return (
-            <>
-              <span className="absolute top-2 right-2 z-10 inline-flex rounded-md shadow-sm">
-                <button
-                  onClick={run}
-                  type="button"
-                  disabled={isLoading || isRunning}
-                  className={clsx(
-                    "relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
-                    !(isLoading || isRunning)
-                      ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
-                      : "opacity-50"
-                  )}
-                >
-                  Run
-                </button>
-                <button
-                  onClick={reset}
-                  type="button"
-                  disabled={isRunning}
-                  className={clsx(
-                    "relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
-                    !isRunning
-                      ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
-                      : "opacity-50"
-                  )}
-                >
-                  Reset
-                </button>
-              </span>
-              {showOutput && (
-                <pre className="mt-4 text-left">
-                  <code>{stdout}</code>
-                  <code className="text-red-500">{stderr}</code>
-                </pre>
-              )}
-            </>
-          );
-        }}
+        {() => (
+          <PythonControl
+            input={input}
+            showOutput={showOutput}
+            onRun={onRun}
+            onStop={onStop}
+            onReset={onReset}
+          />
+        )}
       </BrowserOnly>
     </div>
+  );
+}
+
+interface PythonControlProps {
+  input: string;
+  showOutput: boolean;
+  onRun?: () => void;
+  onStop?: () => void;
+  onReset?: () => void;
+}
+
+function PythonControl(props: PythonControlProps) {
+  const { input, showOutput, onRun, onStop, onReset } = props;
+
+  const { usePython } = require("react-py");
+
+  const {
+    runPython,
+    stdout,
+    stderr,
+    isLoading,
+    isRunning,
+    interruptExecution,
+  } = usePython();
+
+  function run() {
+    runPython(input);
+    onRun && onRun();
+  }
+
+  function stop() {
+    interruptExecution();
+    onStop && onStop();
+  }
+
+  function reset() {
+    runPython(input);
+    onReset && onReset();
+  }
+
+  return (
+    <>
+      <span className="absolute top-2 right-2 z-10 inline-flex rounded-md shadow-sm">
+        {!isRunning ? (
+          <button
+            onClick={run}
+            type="button"
+            disabled={isLoading || isRunning}
+            className={clsx(
+              "relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
+              !isLoading
+                ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
+                : "opacity-50"
+            )}
+          >
+            Run
+          </button>
+        ) : (
+          <button
+            onClick={stop}
+            type="button"
+            className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
+          >
+            Stop
+          </button>
+        )}
+        <button
+          onClick={reset}
+          type="button"
+          disabled={isRunning}
+          className={clsx(
+            "relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700",
+            !isRunning
+              ? "opacity-75 hover:cursor-pointer hover:bg-gray-50 hover:opacity-100"
+              : "opacity-50"
+          )}
+        >
+          Reset
+        </button>
+      </span>
+      {showOutput && (
+        <pre className="mt-4 text-left">
+          <code>{stdout}</code>
+          <code className="text-red-500">{stderr}</code>
+        </pre>
+      )}
+    </>
   );
 }
