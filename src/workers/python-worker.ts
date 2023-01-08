0cff09e57b2a245dbd40f4c8532c9d5c4b20fcb4
Eli Lamb <eli@pixellab.nz>
2023-01-08 23:02:55 +1300

    Upgrade pyodide to 0.22.0, removed reactPyModule

--- src/workers/python-worker.ts
+++ src/workers/python-worker.ts
@@ -1,9 +1,8 @@
-importScripts('https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js')
+importScripts('https://cdn.jsdelivr.net/pyodide/v0.22.0/full/pyodide.js')
 
 interface Pyodide {
   loadPackage: (packages: string[]) => Promise<void>
   pyimport: (pkg: string) => micropip
-  registerJsModule(name: string, module: object): unknown
   runPythonAsync: (code: string) => Promise<void>
   version: string
   FS: {
@@ -35,57 +34,6 @@ console.log = () => {}
 
 import { expose } from 'comlink'
 
-function isSuccessStatus(status: number) {
-  return status >= 200 && status < 300
-}
-
-const reactPyModule = {
-  http: {
-    get: function (url: string) {
-      const xhr = new XMLHttpRequest()
-      xhr.open('GET', url, false)
-      xhr.send()
-      if (isSuccessStatus(xhr.status)) {
-        return xhr.responseText
-      } else {
-        throw new Error(xhr.statusText)
-      }
-    },
-    post: function (url: string, data: JSON) {
-      const xhr = new XMLHttpRequest()
-      xhr.open('POST', url, false)
-      xhr.setRequestHeader('Content-Type', 'application/json')
-      xhr.send(JSON.stringify(data))
-      if (isSuccessStatus(xhr.status)) {
-        return xhr.responseText
-      } else {
-        throw new Error(xhr.statusText)
-      }
-    },
-    put: function (url: string, data: JSON) {
-      const xhr = new XMLHttpRequest()
-      xhr.open('PUT', url, false)
-      xhr.setRequestHeader('Content-Type', 'application/json')
-      xhr.send(JSON.stringify(data))
-      if (isSuccessStatus(xhr.status)) {
-        return xhr.responseText
-      } else {
-        throw new Error(xhr.statusText)
-      }
-    },
-    delete: function (url: string) {
-      const xhr = new XMLHttpRequest()
-      xhr.open('DELETE', url, false)
-      xhr.send()
-      if (isSuccessStatus(xhr.status)) {
-        return xhr.responseText
-      } else {
-        throw new Error(xhr.statusText)
-      }
-    }
-  }
-}
-
 const python = {
   async init(
     stdout: (msg: string) => void,
@@ -103,7 +51,6 @@ const python = {
       const micropip = self.pyodide.pyimport('micropip')
       await micropip.install(packages[1])
     }
-    self.pyodide.registerJsModule('react_py', reactPyModule)
     onLoad(self.pyodide.version)
   },
   async run(code: string) {
